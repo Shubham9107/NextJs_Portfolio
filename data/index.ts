65f82fd7dd61c03dfce36628a3528c76b2424d3a
Shubham Joshi <42163609+Shubham9107@users.noreply.github.com>
2024-08-29 10:10:02 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,96 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Amazon Clone",
+    des: "This platform provides a hands-on demonstration of e-commerce best practices and user experience. It is scalable and adaptable. Leveraged expertise in front-end, back-end development, and database management to develop an e-commerce clone.",
+    img: "/project1.png",
+    iconLists: [],
+    link: "https://amazon-clone-ruddy-five.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Restaurant Management Web Application",
+    des: "Dynamic Restaurant Web Application using React, offering an interactive menu, online reservations, and reviews integration.",
+    img: "/rms.png",
+    iconLists: [],
+    link: "https://restaurant-management-system-tb.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "OS Algorithm Simulator",
+    des: "OS Algorithm Simulator it Provides a Hands-on platform for understanding and experimenting with the core concepts of the Operating System. Implemented Algorithms, like, Shortest Remaining Time Next, Shortest Seek Time First, Producer-Consumer, and Least Recently Used Problem.",
+    img: "/ossim.png",
+    iconLists: [],
+    link: "https://os-projectteam8.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Advanced Auth",
+    des: "Advanced Authentication it provides the Security of Client using MFA or 2FA",
+    img: "/advanced auth.jpg",
+    iconLists: [],
+    link: "/ui.apple.com",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Shubham9107",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/shubham-joshi-567a34236/",
+  },
+];
